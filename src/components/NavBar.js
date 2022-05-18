c25f362ce9d619b4f1b750e5cc74e102ccba5fc5
Micaela Frank <micaelalfrank@gmail.com>
2022-05-18 13:54:25 -0400

    commit tree 433f815efa297bc9aa4af4f39d4cd7b693687253

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkTags = links.map((link) => (
+    <a key={link} href={"#" + link }>
+      {link}
+    </a>
+  ));
+
+  return ( 
+      <nav>
+          {linkTags}
+      </nav>
+  )
 }
 
 export default NavBar;
